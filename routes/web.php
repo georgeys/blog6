4703c8a0ecee1589d110dc46430d77b481597d8e
georgeys <1342479179@qq.com>
2020-06-28 18:36:12 +0800

    '完成登录'

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('test','TestController@index');
+Route::get('/','TestController@index')->name('home');
 //Route::get('/help', 'TestController@help')->name('help');
 //Route::get('/', 'TestController@home')->name('home');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users','UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
