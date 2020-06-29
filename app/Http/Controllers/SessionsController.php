be6cb4351d2c668d7be6ae17ff704d1091d83f51
georgeys <1342479179@qq.com>
2020-06-29 14:03:08 +0800

    '用户激活'

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -42,10 +42,16 @@ class SessionsController extends Controller
       ]);
       if (Auth::attempt($credentials,$request->has('remember')))
       {
-          session()->flash('success','欢迎回来');
-          $fallback =  redirect()->route('users.show',[Auth::user()]);
-          //重定向器上的 intended 方法将用户重定向到登录之前用户想要访问的 URL，在目标 URL 无效的情况下回退 URI 将会传递给该方法。
-          return redirect()->intended($fallback);
+          if(Auth::user()->activated) {
+              session()->flash('success', '欢迎回来');
+              $fallback = redirect()->route('users.show', [Auth::user()]);
+              //重定向器上的 intended 方法将用户重定向到登录之前用户想要访问的 URL，在目标 URL 无效的情况下回退 URI 将会传递给该方法。
+              return redirect()->intended($fallback);
+          }else{
+              Auth::logout();
+              session()->flash('warning','你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+              return redirect('/');
+          }
       }else {
           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
           return redirect()->back()->withInput();
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,14 +6,17 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends Controller
 {
     //
+
+
     public function __construct()
     {
         $this->middleware('auth',[
-            'except' => ['show','create','store','index']
+            'except' => ['show','create','store','index','confirmEmail']
         ]);
     }
 
@@ -59,14 +62,49 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
+
         $user = User::create([
            'name' => $request->name,
            'email' => $request->email,
            'password' => bcrypt($request->password),
         ]);
+
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = '1342479179@qq.com';
+        $name = 'George';
+        $to = $user->email;
+        $subject = "感谢注册 George 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
+    }
+
+    /**
+     * @param $token
+     * @return \Illuminate\Http\RedirectResponse
+     * 邮箱激活功能
+     */
+    public function confirmEmail($token)
+    {
+        //firstOrFail 方法来取出第一个用户
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
         Auth::login($user);
-        session()->flash('success',"欢迎，您将在此开启一段新的旅程");
-        return redirect()->route('users.show',[$user]);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
     }
 
     /**
