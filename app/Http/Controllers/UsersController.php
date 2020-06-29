98cd93f648a7c41e01adbf9937a6f18768971d52
georgeys <1342479179@qq.com>
2020-06-29 09:49:26 +0800

    '更改用户信息'

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class UsersController extends Controller
 {
@@ -21,6 +22,7 @@ class UsersController extends Controller
 
     /**
      * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
      * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request)
@@ -39,5 +41,29 @@ class UsersController extends Controller
         session()->flash('success',"欢迎，您将在此开启一段新的旅程");
         return redirect()->route('users.show',[$user]);
     }
+
+    /**
+     * @param User $user
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit',compact('user'));
+    }
+
+    public function update(User $user,Request $request)
+    {
+        $this->validate($request,[
+            'name' => Rule::unique('users')->ignore($user->id),
+            'password' => 'required|confirmed|min:6'
+        ]);
+        $user->update([
+            'name'      =>  $request->name,
+            'password'  =>  bcrypt($request->password)
+        ]);
+        session()->flash('success',"更新成功");
+        return redirect()->route('users.show',$user->id);
+    }
+
 
 }
