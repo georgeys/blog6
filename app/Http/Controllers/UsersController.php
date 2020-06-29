17f99d826e874e5f4f852e6267f78b9d7446d0cf
georgeys <1342479179@qq.com>
2020-06-29 15:19:09 +0800

    '完成真实的邮件发送'

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -78,13 +78,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '1342479179@qq.com';
-        $name = 'George';
         $to = $user->email;
         $subject = "感谢注册 George 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ( $to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
